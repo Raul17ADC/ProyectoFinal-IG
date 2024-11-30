0dc00f539a809e8fa99462d4de2d63f84673e1a2
Joxem <josgame@hotmail.com>
2024-11-30 17:56:22 +0100

    Mac

--- binary/resources/shaders/vshader.glsl
+++ binary/resources/shaders/vshader.glsl
@@ -1,21 +1,23 @@
 #version 330 core
 
-layout(location = 0)in vec3 inPosition;
-layout(location = 1)in vec3 inNormal;
+layout (location = 0) in vec3 inPosition;
+layout (location = 1) in vec3 inNormal;
 
 uniform mat4 uN;
 uniform mat4 uM;
 uniform mat4 uPVM;
 
-out vec3 vNor;
-out vec3 vPos;
+out vec3 vnor;
+out vec3 vpos;
 
 void main() {
-    vec4 inPositionH = vec4(inPosition, 1);
-    vec4 inNormalH = vec4(inNormal, 0);
-    
+
+    vec4 inPositionH = vec4(inPosition,1);
+    vec4 inNormalH   = vec4(inNormal  ,0);
+
     gl_Position = uPVM * inPositionH;
-    
-    vNor = (uN * inNormalH).xyz;
-    vPos = (uM * inPositionH).xyz;
+
+    vnor = (uN*inNormalH).xyz;
+    vpos = (uM*inPositionH).xyz;
+
 }
