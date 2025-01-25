b318c7db72ffc781f232c780dc1a0d48a123baff
Joxem <josgame@hotmail.com>
2025-01-25 23:29:42 +0100

    Mac

---
+++ build/CMakeFiles/Proyecto_Final.dir/DependInfo.cmake
@@ -0,0 +1,26 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/Users/joxem/Documents/GitHub/ProyectoFinal-IG/src/Model.cpp" "CMakeFiles/Proyecto_Final.dir/src/Model.cpp.o" "gcc" "CMakeFiles/Proyecto_Final.dir/src/Model.cpp.o.d"
+  "/Users/joxem/Documents/GitHub/ProyectoFinal-IG/src/Shaders.cpp" "CMakeFiles/Proyecto_Final.dir/src/Shaders.cpp.o" "gcc" "CMakeFiles/Proyecto_Final.dir/src/Shaders.cpp.o.d"
+  "/Users/joxem/Documents/GitHub/ProyectoFinal-IG/src/Texture.cpp" "CMakeFiles/Proyecto_Final.dir/src/Texture.cpp.o" "gcc" "CMakeFiles/Proyecto_Final.dir/src/Texture.cpp.o.d"
+  "/Users/joxem/Documents/GitHub/ProyectoFinal-IG/src/main.cpp" "CMakeFiles/Proyecto_Final.dir/src/main.cpp.o" "gcc" "CMakeFiles/Proyecto_Final.dir/src/main.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
---
+++ build/CMakeFiles/Proyecto_Final.dir/cmake_clean.cmake
@@ -0,0 +1,17 @@
+file(REMOVE_RECURSE
+  "/Users/joxem/Documents/GitHub/ProyectoFinal-IG/binary/Proyecto_Final"
+  "/Users/joxem/Documents/GitHub/ProyectoFinal-IG/binary/Proyecto_Final.pdb"
+  "CMakeFiles/Proyecto_Final.dir/src/Model.cpp.o"
+  "CMakeFiles/Proyecto_Final.dir/src/Model.cpp.o.d"
+  "CMakeFiles/Proyecto_Final.dir/src/Shaders.cpp.o"
+  "CMakeFiles/Proyecto_Final.dir/src/Shaders.cpp.o.d"
+  "CMakeFiles/Proyecto_Final.dir/src/Texture.cpp.o"
+  "CMakeFiles/Proyecto_Final.dir/src/Texture.cpp.o.d"
+  "CMakeFiles/Proyecto_Final.dir/src/main.cpp.o"
+  "CMakeFiles/Proyecto_Final.dir/src/main.cpp.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/Proyecto_Final.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
